eb34fcb49b4890e5c238244d200bcb0157ee12c9
techpool <suryadeep10@gmail.com>
2017-03-25 01:28:45 +0530

    server port number changed based on environment

--- resources/alexa_apps/server.js
+++ resources/alexa_apps/server.js
@@ -1,8 +1,13 @@
 var AlexaAppServer = require("../index.js");
 
+var port = 8080;
+if (process.env.NODE_ENV == 'production') {
+    port = 80;
+}
+
 AlexaAppServer.start({
     server_root: './',
-    port: 8080,
+    port: port,
     // Use preRequest to load user data on each request and add it to the request json.
     // In reality, this data would come from a db or files, etc.
     preRequest: function(json, req, res) {
